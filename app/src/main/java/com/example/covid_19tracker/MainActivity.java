5c688edb7404ee53e215911f970d324e1d05a3be
AbhaySingh5349 <abhay5349singh@gmail.com>
2021-01-12 01:02:52 +0530

    Displaying Global Statistics

--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.leo.simplearcloader.SimpleArcLoader;
 
 import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -48,5 +61,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        
+        fetchWorldData();
+    }
+
+    private void fetchWorldData() {
+        worldStatsArcLoader.start();
+        String worldDataURL = "https://corona.lmao.ninja/v2/all";
+
+        StringRequest worldDataStringRequest = new StringRequest(Request.Method.GET, worldDataURL, new Response.Listener<String>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+                    totalCasesTextView.setText(jsonObject.getString("cases"));
+                    newTotalCasesTextView.setText("+ " + jsonObject.getString("todayCases"));
+                    activeCasesTextView.setText(jsonObject.getString("active"));
+                    criticalCasesTextView.setText(jsonObject.getString("critical"));
+                    recoveredCasesTextView.setText(jsonObject.getString("recovered"));
+                    newRecoveredCasesTextView.setText("+ " + jsonObject.getString("todayRecovered"));
+                    deceasedCasesTextView.setText(jsonObject.getString("deaths"));
+                    newDeceasedCasesTextView.setText("+ " + jsonObject.getString("todayDeaths"));
+
+                    worldStatsPieChart.addPieSlice(new PieModel("Total Cases",Integer.parseInt(totalCasesTextView.getText().toString()), Color.parseColor("#fed70e")));
+                    worldStatsPieChart.addPieSlice(new PieModel("Active Cases",Integer.parseInt(activeCasesTextView.getText().toString()), Color.parseColor("#56b7f1")));
+                    worldStatsPieChart.addPieSlice(new PieModel("Recovered Cases",Integer.parseInt(recoveredCasesTextView.getText().toString()), Color.parseColor("#63cbb0")));
+                    worldStatsPieChart.addPieSlice(new PieModel("Deceased Cases",Integer.parseInt(totalCasesTextView.getText().toString()), Color.parseColor("#FF0000")));
+
+                    worldStatsPieChart.startAnimation();
+                    worldStatsArcLoader.stop();
+                    worldStatsArcLoader.setVisibility(View.GONE);
+                    cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    worldStatsArcLoader.stop();
+                    worldStatsArcLoader.setVisibility(View.GONE);
+                    cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                worldStatsArcLoader.stop();
+                worldStatsArcLoader.setVisibility(View.GONE);
+                cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                Toast.makeText(MainActivity.this, error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(worldDataStringRequest);
     }
 }
