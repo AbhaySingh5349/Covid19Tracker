cd86a820d0859cbebf1130681b3271e58802cb2a
AbhaySingh5349 <abhay5349singh@gmail.com>
2021-01-13 12:46:53 +0530

    Adding Double Back Press to Exit App

--- app/src/main/java/com/example/covid_19tracker/IndiaDataActivity.java
+++ app/src/main/java/com/example/covid_19tracker/IndiaDataActivity.java
@@ -1,11 +1,13 @@
 package com.example.covid_19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,11 +27,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.NumberFormat;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class IndiaDataActivity extends AppCompatActivity {
 
+    @BindView(R.id.indianStatsCardView)
+    CardView indianStatsCardView;
     @BindView(R.id.indianStatsPieChart)
     PieChart indianStatsPieChart;
     @BindView(R.id.dateTextView)
@@ -77,31 +83,48 @@ public class IndiaDataActivity extends AppCompatActivity {
                     JSONArray stateWiseJsonArray = response.getJSONArray("statewise");
                     JSONObject stateWiseJsonObject = stateWiseJsonArray.getJSONObject(0);
 
+                    String totalCases = stateWiseJsonObject.getString("confirmed");
+                    String todayCases = timeSeriesJsonObject.getString("dailyconfirmed");
+                    String activeCases = stateWiseJsonObject.getString("active");
+                    String recoveredCases = stateWiseJsonObject.getString("recovered");
+                    String newRecovered = timeSeriesJsonObject.getString("dailyrecovered");
+                    String deceasedCases = stateWiseJsonObject.getString("deaths");
+                    String newDeceased = timeSeriesJsonObject.getString("dailydeceased");
+
                     String year = timeSeriesJsonObject.getString("dateymd");
 
                     dateTextView.setText(timeSeriesJsonObject.getString("date") + " " + year.substring(2,4));
-                    totalCasesTextView.setText(stateWiseJsonObject.getString("confirmed"));
-                    newTotalCasesTextView.setText("+ " + timeSeriesJsonObject.getString("dailyconfirmed"));
-                    activeCasesTextView.setText(stateWiseJsonObject.getString("active"));
-                    recoveredCasesTextView.setText(stateWiseJsonObject.getString("recovered"));
-                    newRecoveredCasesTextView.setText("+ " + timeSeriesJsonObject.getString("dailyrecovered"));
-                    deceasedCasesTextView.setText(stateWiseJsonObject.getString("deaths"));
-                    newDeceasedCasesTextView.setText("+ " + timeSeriesJsonObject.getString("dailydeceased"));
+                    totalCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(totalCases)));
+                    newTotalCasesTextView.setText("+ " + NumberFormat.getInstance().format(Integer.parseInt(todayCases)));
+                    activeCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(activeCases)));
+                    recoveredCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(recoveredCases)));
+                    newRecoveredCasesTextView.setText("+ " + NumberFormat.getInstance().format(Integer.parseInt(newRecovered)));
+                    deceasedCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(deceasedCases)));
+                    newDeceasedCasesTextView.setText("+ " + NumberFormat.getInstance().format(Integer.parseInt(newDeceased)));
 
-                    indianStatsPieChart.addPieSlice(new PieModel("Total Cases",Integer.parseInt(totalCasesTextView.getText().toString()), Color.parseColor("#fed70e")));
-                    indianStatsPieChart.addPieSlice(new PieModel("Active Cases",Integer.parseInt(activeCasesTextView.getText().toString()), Color.parseColor("#56b7f1")));
-                    indianStatsPieChart.addPieSlice(new PieModel("Recovered Cases",Integer.parseInt(recoveredCasesTextView.getText().toString()), Color.parseColor("#63cbb0")));
-                    indianStatsPieChart.addPieSlice(new PieModel("Deceased Cases",Integer.parseInt(deceasedCasesTextView.getText().toString()), Color.parseColor("#FF0000")));
+                    indianStatsPieChart.addPieSlice(new PieModel("Total Cases",Integer.parseInt(todayCases), Color.parseColor("#fed70e")));
+                    indianStatsPieChart.addPieSlice(new PieModel("Active Cases",Integer.parseInt(activeCases), Color.parseColor("#56b7f1")));
+                    indianStatsPieChart.addPieSlice(new PieModel("Recovered Cases",Integer.parseInt(recoveredCases), Color.parseColor("#63cbb0")));
+                    indianStatsPieChart.addPieSlice(new PieModel("Deceased Cases",Integer.parseInt(deceasedCases), Color.parseColor("#FF0000")));
 
-                    indianStatsPieChart.startAnimation();
-                    indianStatsArcLoader.stop();
-                    indianStatsArcLoader.setVisibility(View.GONE);
-                    cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            indianStatsPieChart.startAnimation();
+                            indianStatsArcLoader.stop();
+                            indianStatsArcLoader.setVisibility(View.GONE);
+                            indianStatsCardView.setVisibility(View.VISIBLE);
+                            cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                        }
+                    };
+                    handler.postDelayed(runnable,1000);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                     indianStatsArcLoader.stop();
                     indianStatsArcLoader.setVisibility(View.GONE);
+                    indianStatsCardView.setVisibility(View.VISIBLE);
                     cardViewConstraintLayout.setVisibility(View.VISIBLE);
                 }
             }
@@ -110,6 +133,7 @@ public class IndiaDataActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 indianStatsArcLoader.stop();
                 indianStatsArcLoader.setVisibility(View.GONE);
+                indianStatsCardView.setVisibility(View.VISIBLE);
                 cardViewConstraintLayout.setVisibility(View.VISIBLE);
                 Toast.makeText(IndiaDataActivity.this,error.getMessage(),Toast.LENGTH_SHORT).show();
             }
--- app/src/main/java/com/example/covid_19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19tracker/MainActivity.java
@@ -8,6 +8,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +26,7 @@ import org.eazegraph.lib.models.PieModel;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -33,6 +35,8 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.worldStatsCardView)
+    CardView worldStatsCardView;
     @BindView(R.id.worldStatsPieChart)
     PieChart worldStatsPieChart;
     @BindView(R.id.dateTextView)
@@ -62,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.worldDataCardView)
     CardView worldDataCardView;
 
+    private boolean doubleBackPressed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,29 +88,46 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject jsonObject = new JSONObject(response.toString());
 
-                    totalCasesTextView.setText(jsonObject.getString("cases"));
-                    newTotalCasesTextView.setText("+ " + jsonObject.getString("todayCases"));
-                    activeCasesTextView.setText(jsonObject.getString("active"));
-                    criticalCasesTextView.setText(jsonObject.getString("critical"));
-                    recoveredCasesTextView.setText(jsonObject.getString("recovered"));
-                    newRecoveredCasesTextView.setText("+ " + jsonObject.getString("todayRecovered"));
-                    deceasedCasesTextView.setText(jsonObject.getString("deaths"));
-                    newDeceasedCasesTextView.setText("+ " + jsonObject.getString("todayDeaths"));
+                    String totalCases = jsonObject.getString("cases");
+                    String todayCases = jsonObject.getString("todayCases");
+                    String activeCases = jsonObject.getString("active");
+                    String criticalCases = jsonObject.getString("critical");
+                    String recoveredCases = jsonObject.getString("recovered");
+                    String todayRecovered = jsonObject.getString("todayRecovered");
+                    String deceasedCases = jsonObject.getString("deaths");
+                    String newDeceased = jsonObject.getString("todayDeaths");
+
+                    totalCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(totalCases)));
+                    newTotalCasesTextView.setText("+ " + NumberFormat.getInstance().format(Integer.parseInt(todayCases)));
+                    activeCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(activeCases)));
+                    criticalCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(criticalCases)));
+                    recoveredCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(recoveredCases)));
+                    newRecoveredCasesTextView.setText("+ " + NumberFormat.getInstance().format(Integer.parseInt(todayRecovered)));
+                    deceasedCasesTextView.setText(NumberFormat.getInstance().format(Integer.parseInt(deceasedCases)));
+                    newDeceasedCasesTextView.setText("+ " + NumberFormat.getInstance().format(Integer.parseInt(newDeceased)));
 
                     @SuppressLint("SimpleDateFormat")
                     SimpleDateFormat formatter = new SimpleDateFormat("EEE, MMM d, ''yy");
                     String dateString = formatter.format(new Date(Long.parseLong(jsonObject.getString("updated"))));
                     dateTextView.setText(dateString);
 
-                    worldStatsPieChart.addPieSlice(new PieModel("Total Cases",Integer.parseInt(totalCasesTextView.getText().toString()), Color.parseColor("#fed70e")));
-                    worldStatsPieChart.addPieSlice(new PieModel("Active Cases",Integer.parseInt(activeCasesTextView.getText().toString()), Color.parseColor("#56b7f1")));
-                    worldStatsPieChart.addPieSlice(new PieModel("Recovered Cases",Integer.parseInt(recoveredCasesTextView.getText().toString()), Color.parseColor("#63cbb0")));
-                    worldStatsPieChart.addPieSlice(new PieModel("Deceased Cases",Integer.parseInt(deceasedCasesTextView.getText().toString()), Color.parseColor("#FF0000")));
+                    worldStatsPieChart.addPieSlice(new PieModel("Total Cases",Integer.parseInt(todayCases), Color.parseColor("#fed70e")));
+                    worldStatsPieChart.addPieSlice(new PieModel("Active Cases",Integer.parseInt(activeCases), Color.parseColor("#56b7f1")));
+                    worldStatsPieChart.addPieSlice(new PieModel("Recovered Cases",Integer.parseInt(recoveredCases), Color.parseColor("#63cbb0")));
+                    worldStatsPieChart.addPieSlice(new PieModel("Deceased Cases",Integer.parseInt(deceasedCases), Color.parseColor("#FF0000")));
 
-                    worldStatsPieChart.startAnimation();
-                    worldStatsArcLoader.stop();
-                    worldStatsArcLoader.setVisibility(View.GONE);
-                    cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            worldStatsPieChart.startAnimation();
+                            worldStatsArcLoader.stop();
+                            worldStatsArcLoader.setVisibility(View.GONE);
+                            worldStatsCardView.setVisibility(View.VISIBLE);
+                            cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                        }
+                    };
+                    handler.postDelayed(runnable,1000);
 
                     indiaDataCardView.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -118,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
                     worldStatsArcLoader.stop();
                     worldStatsArcLoader.setVisibility(View.GONE);
                     cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                    cardViewConstraintLayout.setVisibility(View.VISIBLE);
                     Toast.makeText(MainActivity.this, e.getMessage(),Toast.LENGTH_LONG).show();
                 }
             }
@@ -127,11 +151,30 @@ public class MainActivity extends AppCompatActivity {
                 worldStatsArcLoader.stop();
                 worldStatsArcLoader.setVisibility(View.GONE);
                 cardViewConstraintLayout.setVisibility(View.VISIBLE);
+                cardViewConstraintLayout.setVisibility(View.VISIBLE);
                 Toast.makeText(MainActivity.this, error.getMessage(),Toast.LENGTH_LONG).show();
             }
         });
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(worldDataStringRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //    super.onBackPressed();
+        if(doubleBackPressed){
+            finishAffinity();
+        }else {
+            doubleBackPressed = true;
+            Toast.makeText(this,"Back Press again to exit the app",Toast.LENGTH_SHORT).show();
+
+            new android.os.Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackPressed = false;
+                }
+            },2000);
+        }
     }
 }
